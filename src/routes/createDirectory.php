8716c4a296bf0e200fab578d2016ba13955be60b
ivz-dev <zakutynsky@yahoo.com>
2017-04-13 15:16:07 +0300

    add text message to createDirectory block

--- src/routes/createDirectory.php
+++ src/routes/createDirectory.php
@@ -35,7 +35,7 @@ $app->post('/api/Algorithmia/createDirectory', function ($request, $response) {
             $result['callback'] = 'success';
             $result['contextWrites']['to'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
             if(empty($result['contextWrites']['to'])) {
-                $result['contextWrites']['to']['status_msg'] = "Api return no results";
+                $result['contextWrites']['to']['status_msg'] = "Directory successfully created";
             }
         } else {
             $result['callback'] = 'error';
